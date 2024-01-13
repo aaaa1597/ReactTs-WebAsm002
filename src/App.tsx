de26701fdffde0b0b1f9f4602236223ce45d0962
aaaa1597 <tokushi2011@gmail.com>
2024-01-13 14:01:39 +0900

    とりあえず、出来た。 なんでか複数回呼ばれるのが腑に落ちんけど。

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { aaa00, aaa01, aaa02 } from './libs/aaaaa';
+import { helloif } from './asm-cpp/helloif';
 
 function App() {
-  console.log('---- ret00 = ', aaa00('aaa000', 0))
-  const persons = aaa01(['aaa001', 'bbb002'], 1)
-  console.log('---- ret01 = ', ...persons)
-  const argbuf = new Uint8Array([11, 22, 33])
-  const ret02 = aaa02(argbuf, 2)
-  console.log('---- ret02 = ', ret02)
+  const [count, setCount] = useState(0)
+  console.log('---------------- asmret=', helloif(11, 22, setCount))
 
   return (
     <div className="App">
       hello world!!
-    </div>
+      <div>count= {count}</div>
+  </div>
   );
 }
 
--- src/libs/aaaaa.d.ts
+++
@@ -1,3 +0,0 @@
-declare export function aaa00(name: string, years: number): string;
-declare export function aaa01(names: string[], years: number): string[];
-declare export function aaa02(datas: Uint8Array, aaa: number): Uint8Array;
